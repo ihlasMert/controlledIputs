b6fe08a802111a19a60747951dce35e3e74a6284
İHLASMERT <ihlas1mert@gmail.com>
2023-01-23 22:16:20 +0300

    created new project froms

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css'
+import MultipleReturns from './multipleReturn'
+
+function App() {
+  return (
+    <div>
+      <MultipleReturns></MultipleReturns>
+    </div>
+  )
+}
+
+export default App
---
+++ src/forms.js
@@ -0,0 +1,7 @@
+import React, { useState } from 'react'
+
+const ControlledInputs = () => {
+  return <div>Controlled Inputs</div>
+}
+
+export default ControlledInputs
